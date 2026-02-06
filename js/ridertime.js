45f63e64c444f547de58448b8498f6e0e7f3d472
LHaupt1s <LHaupt1s@capetigers.com>
2026-02-06 10:38:26 -0600

    continue filling out schedule times

---
+++ js/ridertime.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// Hour names for Red and Black Days
+var longHour = [
+    "1st / 2nd",
+    "3rd / 4th",
+    "5th / 6th",
+    "7th / 8th"
+];
+
+// Beginning Times for Red and Black Days
+var longSchBegin = [
+    "Febuary 6, 2026 7:55:00",
+    "Febuary 6, 2026 9:35:00",
+    "Febuary 6, 2026 11:15:00",
+    "Febuary 6, 2026 13:28:00",
+]
+
+var longSchEnd = [
+    "Febuary 6, 2026 9:30:00",
+    "Febuary 6, 2026 11:10:00",
+    "Febuary 6, 2026 13:23:00",
+    "Febuary 6, 2026 15:03:00",
+]
--- js/schedules.js
+++ js/schedules.js
@@ -3,26 +3,42 @@
 // This function will apply school schedule to the website based off of what option is selected in the dropdown
 
 setupSchedule();
-setInterval("setupSchedule()", 1000);
+document.getElementById("scheduleType").onchange = function() {setupSchedule()};
 
 function setupSchedule() {
 
-    var scheduleHTML = "<div class='can jrsy-font' id='scheduleBox'><h1 id='test'";
+    // Sets up for the heading, which changes colors and text depending on what is selected in the dropdown.
+    var scheduleHTML = "<div class='can jrsy-font' id='scheduleBox'><h1 id='test' ";
 
-    scheduleHTML += "class='rbText'>test</h1>";
+
+    // It changes color first.
+    var classColor = changeColor();
+
+    scheduleHTML += "class='" + classColor + "'>";
+
+
+    var curSchedule = selectSchedule();
+
+    scheduleHTML += curSchedule + "</h1>";
+
     document.getElementById("schedule").innerHTML = scheduleHTML;
+
+    grabTimes();
 
 }
 
-function checkDropdown() {
+function changeColor() {
 
-    var fontRB = "rbText";
-    var fontGr = "greenText";
-    var fontBl = "blueText"
+    // Class Names first!
+    var fontRB = "rbtext";
+    var fontGr = "greentext";
+    var fontBl = "bluetext"
 
+    // Determines the value of what is selected in dropdown.
     const selectedElement = document.getElementById("scheduleType");
     const curSchedule = selectedElement.value;
 
+    // Determines what value it is so it can be set to the correct class name!
     if (curSchedule === "Red / Black Day") {
 
         var classColor = fontRB;
@@ -37,8 +53,53 @@ function checkDropdown() {
 
     }
 
-    
+    return classColor;
 
 }
 
-// document.getElementById("scheduleBox").innerHTML = "<h1 class='jrsy-font' id='testSchedule'>What Schedule are we on?</h1>"
+function selectSchedule() {
+
+    // Grabs Value and returns it.
+    const selectedElement = document.getElementById("scheduleType");
+    const curSchedule = selectedElement.value;
+
+    return curSchedule;
+
+}
+
+function grabTimes() {
+
+    var timeHTML = "";
+
+    for (var i = 0; i < longSchBegin.length; i++) {
+
+        var longDateBegin= new Date(longSchBegin[i]);
+        var longTimeBegin = longDateBegin.toLocaleTimeString();
+
+        var longDateEnd= new Date(longSchEnd[i]);
+        var longTimeEnd = longDateEnd.toLocaleTimeString();
+
+        timeHTML += longHour[i] + " " + longTimeBegin + " - " + longTimeEnd;
+
+    }
+
+    window.alert(timeHTML);
+}
+
+/*  Planning for how to layout html
+<div>
+
+    <h2>longHour[i] + longTime[i]</h2>    time variable will contain both times.
+    <h2>longHour[i] + longTime[i]</h2>
+    <h2>longHour[i] + longTime[i]</h2>
+
+    <div>
+
+        <h3>longLunchHour[0] + longLunchTime[0]</h2>
+        <h3>longLunchHour[1] + longLunchTime[1]</h2>
+        <h3>longLunchHour[2] + longLunchTime[2]</h2>
+        <h3>longLunchHour[3] + longLunchTime[3]</h2>
+
+    </div>
+
+    <h2>longHour[i] + longTime[i]</h2> */
